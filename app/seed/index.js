20bc7768351b098c9718592fea845e14d5c376e8
Jeremy Worboys <jw@jeremyworboys.com>
2013-11-19 23:47:31 +1100

    Truncate tables when there are foreign key constraints

--- app/seed/index.js
+++ app/seed/index.js
@@ -3,6 +3,14 @@ var orm = require('orm');
 var async = require('async');
 var dbConfig = require('../config/database');
 
+var truncate = function(db, table, done) {
+    async.series([
+        async.apply(db.driver.db.query.bind(db.driver.db), 'SET FOREIGN_KEY_CHECKS = 0'),
+        async.apply(db.driver.db.query.bind(db.driver.db), 'TRUNCATE TABLE ' + db.driver.query.escape(table)),
+        async.apply(db.driver.db.query.bind(db.driver.db), 'SET FOREIGN_KEY_CHECKS = 1'),
+    ], done);
+};
+
 module.exports.run = function(done) {
     orm.connect(dbConfig, function(err, db) {
         var cinema = require('../models/cinema')(db);
@@ -10,8 +18,8 @@ module.exports.run = function(done) {
 
         async.series([
             // Truncate tables
-            cinema.clear,
-            movie.clear,
+            async.apply(truncate, db, 'cinemas'),
+            async.apply(truncate, db, 'movies'),
 
             // Seed data
             async.apply(cinema.create, require('./cinemas').cinemas),
