8d8554ae4f7ac0c49bf6f4a7688ba24390c8c1b7
Jeremy Worboys <jw@jeremyworboys.com>
2013-11-20 20:18:24 +1100

    Seed cinemas_movies table

--- app/seed/index.js
+++ app/seed/index.js
@@ -11,19 +11,44 @@ var truncate = function(db, table, done) {
     ], done);
 };
 
+var cinemasMovies = function(db, data, cb) {
+    async.each(data, function(obj, cb) {
+        if (!(obj['cinemas_id'] && obj['movies_id'])) return cb();
+
+        async.series([
+            async.apply(db.models.cinema.get, obj['cinemas_id']),
+            async.apply(db.models.movie.get, obj['movies_id'])
+        ], function(err, results) {
+            if (err) return cb(err);
+            var cinema = results[0];
+            var movie = results[1];
+
+            cinema.addMovies(movie, function(err) {
+                if (err) return cb(err);
+                cinema.save(cb);
+            });
+        });
+    }, cb);
+};
+
 module.exports.run = function(done) {
     orm.connect(dbConfig, function(err, db) {
         var cinema = require('../models/cinema').define(db);
         var movie  = require('../models/movie').define(db);
 
+        require('../models/cinema').associations(db);
+        require('../models/movie').associations(db);
+
         async.series([
             // Truncate tables
             async.apply(truncate, db, 'cinemas'),
             async.apply(truncate, db, 'movies'),
+            async.apply(truncate, db, 'cinemas_movies'),
 
             // Seed data
             async.apply(cinema.create, require('./cinemas').cinemas),
             async.apply(movie.create, require('./movies').movies),
+            async.apply(cinemasMovies, db, require('./cinemas_movies')['cinemas_movies']),
 
         ], function(err) {
             if (err) console.error(err);
